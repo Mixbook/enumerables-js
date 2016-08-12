1f96982e7a29cb83c168853fc64e97945b65cbc8
Dan Schultz <schultz.t.dan@gmail.com>
2016-08-12 00:09:45 -0700

    Rename to enumerables

--- script/build.sh
+++ script/build.sh
@@ -6,5 +6,5 @@ cd "$(dirname "$0")/.."
 ./node_modules/.bin/tsc --module es6
 ./node_modules/.bin/rollup -c
 
-./node_modules/.bin/dts-bundle --main build/src/main.d.ts --out main.d.ts --name "iterables"
+./node_modules/.bin/dts-bundle --main build/src/main.d.ts --out main.d.ts --name "enumerables"
 cp ./build/src/main.d.ts ./dist
